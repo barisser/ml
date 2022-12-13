6ee8e8b2c0adf040f37cf633521d7a7b363e9637
Andrew Barisser <barisser@protonmail.com>
2022-12-12 20:32:28 -0600

    improve terraform configs

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
 }
 
 # Create an EC2 instance
@@ -46,6 +47,24 @@ resource "aws_security_group" "my_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  # jupyter uses 8888 by default
+  ingress {
+    from_port   = 8888
+    to_port     = 8888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # tensorboard uses 6006
+  ingress {
+    from_port   = 6006
+    to_port     = 6006
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -53,3 +72,54 @@ resource "aws_security_group" "my_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# allow our VPC to talk to the public internet
+resource "aws_internet_gateway" "tf" {
+  vpc_id = "${aws_vpc.my_vpc.id}"
+}
+
+
+# this is for the default route table that was created with our VPC
+resource "aws_default_route_table" "rt" {
+  default_route_table_id = "${aws_vpc.my_vpc.default_route_table_id}"
+
+  # make sure all outbound traffic goes through the internet gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.tf.id}"
+  }
+
+}
+
+
+# attach route table to the sbnet
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = "${aws_subnet.my_subnet.id}"
+  route_table_id = "${aws_vpc.my_vpc.default_route_table_id}"
+}
+
+
+
+output "instance_public_ip" {
+  value = "${aws_instance.my_instance.public_ip}"
+}
+
+output "instance_elastic_ip" {
+  value = "${aws_eip.my_eip.public_ip}"
+}
+/*
+resource "aws_ebs_volume" "ebs" {
+  availability_zone = "${aws_instance.my_instance.availability_zone}"
+  size              = 20
+  type              = "gp2"
+
+}
+
+
+resource "aws_ebs_volume_attachment" "example" {
+  device_name = "/dev/sda1"
+  volume_id   = aws_ebs_volume.ebs.id
+  instance_id = aws_instance.my_instance.id
+}
+*/
+
